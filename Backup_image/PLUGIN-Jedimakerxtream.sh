18ea99364f09a9358f982f2d4800f8f765c55936
KhaledAli65 <129581838+KhaledAli65@users.noreply.github.com>
2026-07-17 14:51:25 +0300

    Add files via upload

--- Backup_image/PLUGIN-Jedimakerxtream.sh
+++ Backup_image/PLUGIN-Jedimakerxtream.sh
@@ -4,7 +4,7 @@
 
 # --- الإعدادات ---
 # ضع الرابط الخاص بك بين علامتي التنصيص بالأسفل
-MANUAL_URL="https://github.com/KhaledAli65/plugins/raw/refs/heads/main/ipk_packages/jedimakerxtream.ipk"
+MANUAL_URL="https://github.com/KhaledAli65/Slot/releases/download/v1.0.0/jedimakerxtream.ipk"
 
 # إذا تم تمرير رابط كمتغير أول من البلجن، سيتم استخدامه بدلاً من الرابط اليدوي
 URL=${1:-$MANUAL_URL}
